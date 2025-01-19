c59802d6dd90214e9bfa938436e563e365ef5c1b
Abhishek Sangam <abhishek.sangam@harman.com>
2025-01-20 00:03:33 +0530

    Added popup prefabs Added readme info (in progress)

--- ChestSystem/Assets/Scripts/Chests/Chest/AnimatedChests/AnimatedRareChest.cs
+++ ChestSystem/Assets/Scripts/Chests/Chest/AnimatedChests/AnimatedRareChest.cs
@@ -6,23 +6,40 @@ public class AnimatedRareChest : AnimatedChest
     [SerializeField] private Image ChestImage;
     [SerializeField] private Sprite LockedSprite;
     [SerializeField] private Sprite UnlockedSprite;
+    [SerializeField] private GameObject LoadingSprite;
+
+    private bool isUnlocking = false;
     public override void ShowLocked()
     {
         ChestImage.sprite = LockedSprite;
+        LoadingSprite.SetActive(false);
+        isUnlocking = false;
     }
 
     public override void ShowUnlocked()
     {
         ChestImage.sprite = UnlockedSprite;
+        LoadingSprite.SetActive(false);
+        isUnlocking = false;
     }
 
     public override void ShowUnlocking()
     {
-        Debug.Log("ShowUnlocking");
+        LoadingSprite.SetActive(true);
+        isUnlocking = true;
     }
 
     public override void PlayOpenAnimation()
     {
-        Debug.Log("PlayOpenAnimation");
+        LoadingSprite.SetActive(false);
+        isUnlocking = false;
+    }
+
+    private void Update()
+    {
+        if (isUnlocking)
+        {
+            LoadingSprite.transform.Rotate(0, 0, 1);
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# ChestSystem
-Clash Royale - like chest system using state pattern
+# TradingSystem
+### Summary
+A scalable game mechanic for in-game trading between player and the shop. 
+
+### Gameplay demo
+
+
+## Architecture overview
+### Block diagram
+![Block diagram](./classDiagram/classDiagram.png)
+
+### ChestSlotsController
+
+### ChestController
+
+
+### ChestView and AnimatedChest
+
+### ChestStateMachine and ChestState 
+
+### ChestCommand
+
+### GameConfig
+Hold game's global config constants.
+
+### UI service
+`UIService` class is a singleton that manages warning popups and game's main UI.
+
+### PlayerController
+`PlayerController` manages player's state and provides interfaces for managing player's resources.
